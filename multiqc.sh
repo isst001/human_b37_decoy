91fec3897abfafb2947f129bdd1740244ff86ba0
isst001 <46483721+isst001@users.noreply.github.com>
2022-03-15 10:11:05 +0900

    Update multiqc.sh

--- multiqc.sh
+++ multiqc.sh
@@ -3,14 +3,14 @@
 #PJM -j
 #PJM -s
 
-LOGIN_NODE_EXEC_DIR="/home/z44331r/"
+LOGIN_NODE_EXEC_DIR="/home/z44331r"
 
-login_node_out_dir=${LOGIN_NODE_EXEC_DIR} # LOGIN_NODE_EXEC_DIR='2107241442_E100009254'; SAMPLE_NAME='B000209'
+login_node_out_dir=${LOGIN_NODE_EXEC_DIR}/furo_ngs/mitsu_fastq/multiqc_results # LOGIN_NODE_EXEC_DIR='2107241442_E100009254'; SAMPLE_NAME='B000209'
 exec_node_out_dir=${PJM_LOCALDIR}
 
 SINGULARITY_CONTAINER="/home/z44331r/furo_ngs/singularity_containers/Germline_pipeline_v0.12.sif"
 singularity_bind_path=${PJM_LOCALDIR}
-FASTQC_ORIGIN_DIR=${login_node_out_dir}/furo_ngs/mitsu_fastq/
+FASTQC_ORIGIN_DIR="/home/z44331r/furo_ngs/mitsu_fastq/"
 
 cp -r ${FASTQC_ORIGIN_DIR} ${PJM_LOCALDIR}
 cp -r ${SINGULARITY_CONTAINER} ${PJM_LOCALDIR}
@@ -19,12 +19,12 @@ cd ${PJM_LOCALDIR}
 module load singularity
 
 #PJM_LOCALDIR（/local/jobID)のときはbind設定は、 --bind $PWD:$PWD 
-singularity exec --bind $PWD:$PWD Germline_pipeline_v0.12.sif multiqc ${exec_node_out_dir}
+singularity exec --bind $PWD:$PWD Germline_pipeline_v0.12.sif multiqc ${PJM_LOCALDIR}
 
 #xx.html
 
-cp -r ${exec_node_out_dir}/*.html ${login_node_out_dir}/furo_ngs/mitsu_fastq/multiqc_results
+cp -r ${exec_node_out_dir}/*.html ${login_node_out_dir}
 wait
-rm -rf ${exec_node_out_dir}/*
+rm -rf ${PJM_LOCALDIR}/*
 
 exit0
